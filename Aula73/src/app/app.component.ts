c6bc07b47cd4570c44a55cb8df7c98a69c70fc18
Faesacc2021 <103604848+Faesacc2021@users.noreply.github.com>
2025-10-03 06:49:40 -0300

    (save login)

--- Aula73/src/app/app.component.ts
+++ Aula73/src/app/app.component.ts
@@ -1,11 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
+
+// Função de validação customizada
+function passwordMatchValidator(control: AbstractControl): ValidationErrors | null {
+  const password = control.get('password');
+  const confirmPassword = control.get('confirmPassword');
+
+  if (password && confirmPassword && password.value !== confirmPassword.value) {
+    return { passwordMismatch: true };
+  }
+
+  return null;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  loginForm!: FormGroup;
   fontSize: number = 15;
   textColor: 'white' | 'orange' = 'white';
   buttonText: 'Branco' | 'Laranja' = 'Laranja';
@@ -14,6 +28,29 @@ export class AppComponent {
     'font-size': this.fontSize + 'px',
     'color': this.textColor,
   };
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      confirmPassword: new FormControl('', [Validators.required])
+    }, { validators: passwordMatchValidator });
+  }
+
+  get name() { return this.loginForm.get('name'); }
+  get email() { return this.loginForm.get('email'); }
+  get password() { return this.loginForm.get('password'); }
+  get confirmPassword() { return this.loginForm.get('confirmPassword'); }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      console.log('Formulário válido!', this.loginForm.value);
+      // Aqui você pode enviar os dados para um servidor
+    } else {
+      console.log('Formulário inválido!');
+    }
+  }
 
   increaseFontSize() {
     this.fontSize += 5;
--- Aula73/src/app/app.module.ts
+++ Aula73/src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms'; // Importe o ReactiveFormsModule
+
 import { AppComponent } from './app.component';
-
-
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule // Adicione aqui
   ],
   providers: [],
   bootstrap: [AppComponent]
